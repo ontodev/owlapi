c2374c3cf3569bc4d81f3d7335bcf2eb0246951d
James A. Overton <james@overton.ca>
2014-05-17 10:10:18 -0400

    Bumping dependency versions.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/ontodev/owlapi"
   :license {:name "Simplified BSD License"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.4"]
-                 [net.sourceforge.owlapi/owlapi-api "3.4.10"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [net.sourceforge.owlapi/owlapi-api "3.5.0"]
                  [com.hermit-reasoner/org.semanticweb.hermit "1.3.8.4"]
-                 [org.codehaus.woodstox/woodstox-core-asl "4.1.4"]]
+                 [org.codehaus.woodstox/woodstox-core-asl "4.3.0"]]
   :profiles
   {:dev {:plugins [[lein-marginalia "0.7.1"]]}})
