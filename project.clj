4c7a20aedd66387105b67aa82b4435962c9766eb
James A. Overton <james@overton.ca>
2014-06-04 11:06:30 -0400

    Bump version number and update docs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ontodev/owlapi "0.3.1-SNAPSHOT"
+(defproject ontodev/owlapi "0.3.2"
   :description "A thin Clojure wrapper around OWLAPI."
   :url "http://github.com/ontodev/owlapi"
   :license {:name "Simplified BSD License"
